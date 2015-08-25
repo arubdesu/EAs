0b63f809793e1d93bc72c94a7ac2ba9a1e1d136c
Allister Banks <arubdesu@me.com>
2015-08-25 15:56:26 -0400

    more tweakage
    remember that rc.server hacI MEAN FIX? This makes sure it just has that
    bootCacheJettison flag only.

--- kexts.py
+++ kexts.py
@@ -36,6 +36,7 @@ def main():
                "/System/Library/Extensions/dne.kext",
                "/System/Library/Extensions/dniregistry.kext",
                "/System/Library/Extensions/net6im.kext",
+               "/System/Library/Extensions/Seagate Storage Driver.kext",
                "/System/Library/Extensions/Soundflower.kext",]
 
     cmd = ['/usr/local/bin/osqueryi', '--json', 'select name, path from kernel_extensions']
--- rc_colas.py
+++ rc_colas.py
@@ -12,7 +12,8 @@ def main():
                          "/etc/rc.common~previous" : 'c2c9d79c11496a2bd59b9cec11bc5b6817098303',
                          "/etc/rc.deferred_install" : '0c64ec4dfe03ccc8523a5872acf1bbcf48199f6a',
                          "/etc/rc.imaging" : '6d67f95a31b36116c5e4d1d09ff4cc03e046db60',
-                         "/etc/rc.netboot" : '471e633b1f6bb3de9a48147ebfa5858f71ab0c32'}
+                         "/etc/rc.netboot" : '471e633b1f6bb3de9a48147ebfa5858f71ab0c32',
+                         "/etc/rc.server" : '6dba02aa6e6f5eb9d9dc97bdd3dcef256a698163',}
     present_rcs = glob.glob('/etc/rc*')
     caught_rcs = []
     for cola in present_rcs:#expected_rc_colas.keys():
